9407ca191c2f58c6b2d0cb4102e3f16d59a13a1e
joana.deluca <joana.deluca@senior.com.br>
2017-07-06 13:18:32 -0300

    All events

---
+++ app/controllers/events.controller.js
@@ -0,0 +1,27 @@
+module.exports = {
+  
+  // show all events
+  showEvents: (req, res) => {
+    // create dummy events
+    const events = [
+      {
+        name: 'Basketball', 
+        slug: 'basketball',
+        description: 'Throwing into a basket.'
+      },
+      {
+        name: 'Swimming', 
+        slug: 'swimming',
+        description: 'Michael Phelps is the fast fish.'
+      },
+      {
+        name: 'Weightlifting', 
+        slug: 'weightlifting',
+        description: 'Lifting heavy things up'
+      }
+    ];
+
+    // return a view with data
+    res.render('pages/events', { events: events });
+  }
+};
--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,14 @@
 const express = require('express'),
       router = express.Router(),
       mainController = require('./controllers/main.controller');
+      eventsController = require('./controllers/events.controller')
 
 // export router
 module.exports = router;
 
 // define routes
+// main routes
 router.get('/', mainController.showHome);
+
+// events routes
+router.get('/events', eventsController.showEvents);
